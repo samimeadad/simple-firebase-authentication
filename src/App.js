2c76ef13ef6fe327ab9224cf361c1fc1b705542b
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-10-09 13:51:03 +0600

    google and github authentication done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.initialize';
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, signOut } from "firebase/auth";
+import { useState } from 'react';
 
-function App() {
+initializeAuthentication();
+
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
+function App () {
+  const [ user, setUser ] = useState( {} );
+  const [ errors, setErrors ] = useState( {} );
+  const auth = getAuth();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup( auth, googleProvider )
+      .then( result => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          image: photoURL
+        };
+        setUser( loggedInUser );
+      } )
+      .catch( error => {
+        const { message } = error;
+        const errorMessage = {
+          message: message
+        };
+        setErrors( errorMessage );
+      } );
+  }
+
+  const handleGithubSingIn = () => {
+    signInWithPopup( auth, githubProvider )
+      .then( result => {
+        const { displayName, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          image: photoURL
+        };
+        setUser( loggedInUser );
+      } )
+      .catch( error => {
+        const { message } = error;
+        const errorMessage = {
+          message: message
+        };
+        setErrors( errorMessage );
+      } );
+  }
+
+  const handleSignOut = () => {
+    signOut( auth )
+      .then( () => {
+        // Sign-out successful.
+        setUser( {} );
+      } ).catch( ( error ) => {
+        // An error happened.
+      } );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { !user.name ?
+        <div>
+          <button onClick={ handleGoogleSignIn }>Google Sign In</button>
+          <button onClick={ handleGithubSingIn }>Github Sign In</button>
+        </div> :
+        <button onClick={ handleSignOut }>Sign Out</button>
+      }
+
+      <br />
+      {
+        user.name && <div>
+          <h2>Welcome { user.name }</h2>
+          <p>I know your email address: { user.email }</p>
+          <img src={ user.image } alt="" />
+        </div>
+      }
+      {
+        errors.message && <div>
+          <h2>{ errors.message }</h2>
+        </div>
+      }
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD2cZ_G71nN5gBuPxTv5-g1bn0DewLozE4",
+    authDomain: "simple-firebase-authenti-c494a.firebaseapp.com",
+    projectId: "simple-firebase-authenti-c494a",
+    storageBucket: "simple-firebase-authenti-c494a.appspot.com",
+    messagingSenderId: "292407699025",
+    appId: "1:292407699025:web:ef3b590fe114076521fa70"
+};
+
+export default firebaseConfig;
